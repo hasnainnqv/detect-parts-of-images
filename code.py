b066602dd2c3df7ffe35b9a279a19f992ebb7266
syed hasnain ali naqvi <104722638+hasnainnqv@users.noreply.github.com>
2023-02-27 19:24:29 +0500

    file.py

---
+++ code.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+# from matplotlib import pyplot as plt
+img_rgb = cv.imread('mario1.jpg')
+img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+template = cv.imread('mario_coin.jpg',0)
+w, h = template.shape[::-1]
+res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    print(pt)
+    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
+# cv.imshow("img1",img_rgb)
+# cv.waitKey(0)
+# print(loc[::-1][0])
+# print("shape", template.shape)
+cv.imshow("res",res)
+cv.imshow('original',img_rgb)
+cv.waitKey(0)
